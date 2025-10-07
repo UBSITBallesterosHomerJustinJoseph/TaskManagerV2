65fcebdf07ed27756787d622ac2bef35dbcd312c
Homer Ballesteros <ballesteros_ho@ca01.ub.lan>
2025-10-07 12:55:27 +0800

    test commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { TaskForm } from './task-form/task-form';
+import { TaskList } from './task-list/task-list';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [TaskForm, TaskList, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('taskmanager');
+  protected readonly title = signal('task-manage');
 }
